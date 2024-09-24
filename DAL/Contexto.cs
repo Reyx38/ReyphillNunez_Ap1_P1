d10a052f1d3337315ac8208e595e2df587cec88e
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-23 21:09:38 -0400

    Se crearon los archivos del contexto, registro y el archivo vacio del registroservices

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReyphillNunez_Ap1_P1.Models;
+
+namespace ReyphillNunez_Ap1_P1.DAL;
+
+    public class Contexto : DbContext
+    {
+        DbSet<Registros> Registros { get; set; }
+
+		public Contexto(DbContextOptions<Contexto> options) : base(options)
+		{
+		}
+	}
+
---
+++ Services/RegistroService.cs
@@ -0,0 +1,6 @@
+﻿namespace ReyphillNunez_Ap1_P1.Services
+{
+    public class RegistroService
+    {
+    }
+}
