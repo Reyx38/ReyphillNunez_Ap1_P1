a998fbc5841248b23d603e143b6c7d580cda3028
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-30 19:46:15 -0400

    cambio en modelo

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -7,13 +7,14 @@ namespace ReyphillNunez_Ap1_P1.Models
         [Key]
         [Required]
         public int DeudorId { get; set; }
-        [Required]
+        [Required(ErrorMessage =" Campo obligatorio")]
         [StringLength(50)]
         public string? Nombres { get; set; }
-        [Required]
+        [Required(ErrorMessage = " Campo obligatorio")]
         [StringLength(100)]
         public string? Concepto { get; set; }
-        [Required]
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [Range(1,double.MaxValue)]
         public int Deuda {  get; set; }
         
 
