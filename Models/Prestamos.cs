30e566b997e8df790838b53e01fd81847857bb94
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-30 20:54:18 -0400

    Se agrego la validacion de solo letras

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -9,10 +9,12 @@ namespace ReyphillNunez_Ap1_P1.Models
         public int DeudorId { get; set; }
         [Required(ErrorMessage =" Campo obligatorio")]
         [StringLength(50)]
-        public string? Nombres { get; set; }
+		[RegularExpression("^[a-zA-ZÀ-ÿ\\s]+$", ErrorMessage = "Solo se permiten letras.")]
+		public string? Nombres { get; set; }
         [Required(ErrorMessage = " Campo obligatorio")]
         [StringLength(100)]
-        public string? Concepto { get; set; }
+		[RegularExpression("^[a-zA-ZÀ-ÿ\\s]+$", ErrorMessage = "Solo se permiten letras.")]
+		public string? Concepto { get; set; }
         [Required(ErrorMessage = " Campo obligatorio")]
         [Range(1,double.MaxValue)]
         public int Deuda {  get; set; }
