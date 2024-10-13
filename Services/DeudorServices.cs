9ead42d2f8a79f83e4f249bfde9972f4cdf26737
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-10-12 21:54:46 -0400

    Se agregaron las tablase deudores, cobres y cobros detalles, aparte de hacer del model creating

---
+++ Services/DeudorServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReyphillNunez_Ap1_P1.DAL;
+using ReyphillNunez_Ap1_P1.Models;
+using System.Linq.Expressions;
+
+namespace ReyphillNunez_Ap1_P1.Services
+{
+	public class DeudorServices(Contexto contexto)
+	{
+		private readonly Contexto _contexto = contexto;
+
+		public async Task<List<Deudores>> Listar(Expression<Func<Deudores,bool>> criterio)
+		{
+			return await _contexto.Deudores
+				.AsNoTracking().Where(criterio)
+				.ToListAsync();
+		}
+	}
+}
