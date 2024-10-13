72cffe77974c4db0771cdeca676c9b1e60b1cf60
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-10-12 21:53:39 -0400

    Cambios en la base de dato

--- Migrations/20241010191440_Initial.Designer.cs
+++ Migrations/20241010191440_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReyphillNunez_Ap1_P1.DAL;
 
@@ -10,9 +11,11 @@ using ReyphillNunez_Ap1_P1.DAL;
 namespace ReyphillNunez_Ap1_P1.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20241010191440_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
