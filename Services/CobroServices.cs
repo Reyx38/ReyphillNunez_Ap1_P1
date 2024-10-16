9843bd751155314ca05f82f40a3c8fd8d0d3e8f0
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-10-15 23:06:21 -0400

    Cambios en el eliminar, cambios en el insertar y se agrego el filescopenamespaces

--- Services/CobroServices.cs
+++ Services/CobroServices.cs
@@ -1,53 +1,80 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using ReyphillNunez_Ap1_P1.DAL;
 using ReyphillNunez_Ap1_P1.Models;
 using System.Linq.Expressions;
 
-namespace ReyphillNunez_Ap1_P1.Services
+namespace ReyphillNunez_Ap1_P1.Services;
+
+public class CobroServices(Contexto contexto)
 {
-	public class CobroServices(Contexto contexto)
+	private readonly Contexto _contexto = contexto;
+
+	public async Task<bool> ExisteId(int id)
 	{
-		private readonly Contexto _contexto = contexto;
+		return await _contexto.Cobros.AnyAsync(t => t.CobroId == id);
+	}
 
-		public async Task<bool> ExisteId(int id)
-		{
-			return await _contexto.Cobros.AnyAsync(t => t.CobroId == id);
-		}
+	private async Task<bool> Insertar(Cobros cobros)
+	{
+		await AfectarCobro(cobros.CobrosDetalles.ToArray());
+		_contexto.Cobros.Add(cobros);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
 
-		private async Task<bool> Insertar(Cobros cobros)
-		{
-			_contexto.Cobros.Add(cobros);
-			return await _contexto.SaveChangesAsync() > 0;
-		}
+	private async Task<bool> Modificar(Cobros cobros)
+	{
+		_contexto.Update(cobros);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
 
-		private async Task<bool> Modificar(Cobros cobros)
-		{
-			_contexto.Update(cobros);
-			return await _contexto.SaveChangesAsync() > 0;
-		}
+	public async Task<bool> Guardar(Cobros cobros)
+	{
+		if (!await ExisteId(cobros.CobroId))
+			return await Insertar(cobros);
 
-		public async Task<bool> Guardar(Cobros cobros)
-		{
-			if (!await ExisteId(cobros.CobroId))
-				return await Insertar(cobros);
+		return await Modificar(cobros);
+	}
+	public async Task<bool> Delete(int id)
+	{
+		var cobro =  _contexto.Cobros.Find(id);
+		
+		await AfectarCobro(cobro.CobrosDetalles.ToArray(), false);
 
-			return await Modificar(cobros);
-		}
-		public async Task<bool> Delete(int id)
-		{
-			return await _contexto.Cobros
-				.Where(t => t.CobroId == id).ExecuteDeleteAsync() > 0;
-		}
-		public async Task<Cobros?> Buscar(int id)
-		{
-			return await _contexto.Cobros.Include(c => c.CobrosDetalles)
-				.FirstOrDefaultAsync(c => c.CobroId == id);
-		}
+		_contexto.CobrosDetalles.RemoveRange(cobro.CobrosDetalles);
+		_contexto.Cobros.Remove(cobro);
 
-		public async Task<List<Cobros>> Listar(Expression<Func<Cobros, bool>> criterio)
+		var cantidad =  await _contexto.SaveChangesAsync();
+		return cantidad > 0;
+		
+	}
+	public async Task<Cobros?> Buscar(int id)
+	{
+		return await _contexto.Cobros.Include(c => c.CobrosDetalles)
+			.FirstOrDefaultAsync(c => c.CobroId == id);
+	}
+
+	public async Task<List<Cobros>> Listar(Expression<Func<Cobros, bool>> criterio)
+	{
+		return await _contexto.Cobros.Include(c => c.CobrosDetalles)
+			.AsNoTracking().Where(criterio).ToListAsync();
+	}
+
+	private async Task AfectarCobro(CobrosDetalles[] detalles, bool resta = true)
+	{
+
+		foreach (var item in detalles)
 		{
-			return await _contexto.Cobros.Include(c => c.CobrosDetalles)
-				.AsNoTracking().Where(criterio).ToListAsync();
+			var prestamo = await _contexto.Prestamos.SingleAsync(p => p.PrestamoId == item.PrestamoId);
+			if (resta)
+			{
+				prestamo.Balance -= item.ValorCobrado;
+			}
+			else
+			{
+				prestamo.Balance += item.ValorCobrado;
+			}
+
 		}
 	}
 }
